94bcb00e1f2191283e71938a8ae4f83ded52fceb
MR Ferry <ferry.suhandri@pji-solutions.com>
2022-07-03 21:19:33 +0700

    feat: first commit

---
+++ src/main/java/app/netlify/ferry/example/dynamic_query/persistence/StudentMajor.java
@@ -0,0 +1,9 @@
+package app.netlify.ferry.example.dynamic_query.persistence;
+
+public enum StudentMajor{
+	IPA,
+	IPS,
+	BAHASA,
+	NONE,
+	;
+}
---
+++ src/main/java/app/netlify/ferry/example/dynamic_query/projection/StudentNikAndNameProjection.java
@@ -0,0 +1,10 @@
+package app.netlify.ferry.example.dynamic_query.projection;
+/************************
+ * Made by [MR Ferry™]  *
+ * on July 2022         *
+ ************************/
+
+public interface StudentNikAndNameProjection{
+	String getNik();
+	String getStudentName();
+}
